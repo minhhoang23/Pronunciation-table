baf98c3925c45ff0f2add28dcda92afc90beb15f
Minh Hoang <minh113311@gmail.com>
2017-09-09 00:18:09 +0700

    Pronunciation table - switched to option_box branch: added option box with button, script doesn't work yet.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+$('.overlay').hide();
+
+//event handler for showing the overlay
 $('a').click(function (e) {
 	'use strict';
 	e.preventDefault();
 	var audio = new Audio("sounds/" + this.id + ".mp3");
 	audio.play();
+	$('.overlay').show();
+	//add image and class to the image
+	$('.option-box').prepend("<img src = 'images/" + this.id + ".png'>");
+	$('.option-box img').addClass('image');
+	return false;
+});
+
+//event handler for hiding the overlay
+$('.overlay').click(function (e) {
+	'use strict';
+	e.preventDefault();
+	//e.stopPropagation(;)
+	//excluding elements of "box" from triggering the hide event
+	if (!$(e.target).is('.option-box, .btn, .image, .button, .example')) {
+		$(this).hide();
+		$('.option-box img').remove();
+	}
+});
+
+//event handler for showing examples
+$('#example').click(function (e) {
+	'use strict';
+	e.preventDefault();
+	$('.example').slideToggle(200);
 });
